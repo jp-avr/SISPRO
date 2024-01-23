3cd1df31441e4e8b476d627708d1de2da0b6daff
jp-avr <joaopedroazevedoreisveras@gmail.com>
2024-01-23 17:56:03 -0300

    alterações request

--- app/Http/Requests/ProcessoAdministrativoInserirRequest.php
+++ app/Http/Requests/ProcessoAdministrativoInserirRequest.php
@@ -24,6 +24,8 @@ class ProcessoAdministrativoInserirRequest extends FormRequest
     public function rules()
     {
         return [
+            'cid_id' => ['required','integer'],
+            'estado_civil_id' => ['required','integer'],
             'cliente_nome' => ['required','string'],
             'cliente_data_nascimento' => ['required','date'],
             'cliente_sexo' => ['required','string'],
@@ -31,17 +33,14 @@ class ProcessoAdministrativoInserirRequest extends FormRequest
             'cliente_cpf' => ['required','string'],
             'cliente_email' => ['required','email'],
             'cliente_telefone' => ['required','string'],
-            'cliente_estado_civil' => ['required','string'],
-            'proc_adm_numero_proc' => ['required','string'],
-            'proc_adm_nome_perito' => ['required','string'],
-            // 'proc_adm_numero_req' => ['required','string'],
-            // 'proc_adm_data_exame' => ['required','date'],
-            'proc_adm_cid' => ['required','string'],
-            'proc_adm_data_ini_doenca' => ['required','date'],
-            'proc_adm_data_ini_incapacidade' => [ 'required','date'],
-            'proc_adm_parte_atingida' => ['required','string'],
-            'proc_adm_historia' => [ 'required','string'],     
-            'proc_adm_consideracoes' => ['string', 'nullable']      
+            'proc_numero_proc' => ['required','string'],
+            'proc_nome_perito' => ['required','string'],
+
+            'proc_data_ini_doenca' => ['required','date'],
+            'proc_data_ini_incapacidade' => [ 'required','date'],
+            'proc_parte_atingida' => ['required','string'],
+            'proc_historia' => [ 'required','string'],     
+            'proc_consideracoes' => ['string', 'nullable']      
         ];
     }
 
@@ -66,38 +65,40 @@ class ProcessoAdministrativoInserirRequest extends FormRequest
 
             'cliente_email.string' => 'O campo Email é inválido',
             'cliente_email.required' => 'O campo Email é obrigatório',
-            'cliente_estado_civil.integer' => 'O campo Estado Civil é inválido',
-            'cliente_estado_civil.required' => 'O campo Estado Civil é obrigatório',
+
+            'estado_civil_id.integer' => 'O campo Estado Civil é inválido',
+            'estado_civil_id.required' => 'O campo Estado Civil é obrigatório',
+            
             'cliente_telefone.string' => 'O campo Telefone é inválido',
             'cliente_telefone.required' => 'O campo Telefone é obrigatório',
 
             //--------------------- PROCESSOS -------------------------------------------
-            'proc_adm_numero_proc.string' => 'O campo Nº do Processo é inválido',
-            'proc_adm_numero_proc.required' => 'O campo Nº do Processo é obrigatório',
+            'proc_numero_proc.string' => 'O campo Nº do Processo é inválido',
+            'proc_numero_proc.required' => 'O campo Nº do Processo é obrigatório',
 
-            'proc_adm_nome_perito.string' => 'O campo Médico é inválido',
-            'proc_adm_nome_perito.required' => 'O campo Médico é obrigatório',
+            'proc_nome_perito.string' => 'O campo Médico é inválido',
+            'proc_nome_perito.required' => 'O campo Médico é obrigatório',
 
-            // 'proc_adm_data_exame.date' => 'O campo Data do Exame é inválido',
-            // 'proc_adm_data_exame.required' => 'O campo Data do Exame é obrigatório',
+            // 'proc_data_exame.date' => 'O campo Data do Exame é inválido',
+            // 'proc_data_exame.required' => 'O campo Data do Exame é obrigatório',
 
-            'proc_adm_cid.string' => 'O campo CID10 é inválido',
-            'proc_adm_cid.required' => 'O campo CID10 é obrigatório',
+            'cid_id.integer' => 'O campo CID10 é inválido',
+            'cid_id.required' => 'O campo CID10 é obrigatório',
 
-            'proc_adm_data_ini_doenca.date' => 'O campo Data Inicial dos Sintomas é inválido', 
-            'proc_adm_data_ini_doenca.required' => 'O campo Data Inicial dos Sintomas é obrigatório',
+            'proc_data_ini_doenca.date' => 'O campo Data Inicial dos Sintomas é inválido', 
+            'proc_data_ini_doenca.required' => 'O campo Data Inicial dos Sintomas é obrigatório',
 
-            'proc_adm_data_ini_incapacidade.date' => 'O campo Data Inicial dos Sintomas é inválido',
-            'proc_adm_data_ini_incapacidade.required' => 'O campo Data Inicial dos Sintomas é obrigatório',
+            'proc_data_ini_incapacidade.date' => 'O campo Data Inicial dos Sintomas é inválido',
+            'proc_data_ini_incapacidade.required' => 'O campo Data Inicial dos Sintomas é obrigatório',
 
-            'proc_adm_parte_atingida.string' => 'O campo Parte do corpo atingida é inválido',   
-            'proc_adm_parte_atingida.required' => 'O campo Parte do corpo atingida é obrigatório',
+            'proc_parte_atingida.string' => 'O campo Parte do corpo atingida é inválido',   
+            'proc_parte_atingida.required' => 'O campo Parte do corpo atingida é obrigatório',
 
-            'proc_adm_historia.string' => 'O campo História é inválido',
-            'proc_adm_historia.required' => 'O campo História é obrigatório',
+            'proc_historia.string' => 'O campo História é inválido',
+            'proc_historia.required' => 'O campo História é obrigatório',
 
-            'proc_adm_consideracoes.string' => 'O campo Considerações é inválido',
-            'proc_adm_consideracoes.required' => 'O campo Considerações é obrigatório',  
+            'proc_consideracoes.string' => 'O campo Considerações é inválido',
+            'proc_consideracoes.required' => 'O campo Considerações é obrigatório',  
         ];
     }
 }
