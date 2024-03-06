f08612e5aba8860b42acc6a48f988eee49281bc3
João Pedro Azevedo Veras dos Reis <joaopedroazevedoreisveras@gmail.com>
2024-03-06 18:18:15 -0300

    request do formulário

--- app/Http/Requests/FormularioRequest.php
+++ app/Http/Requests/FormularioRequest.php
@@ -40,7 +40,39 @@ class FormularioRequest extends MessagesRequest
 
     public function messages()
     {
-        return parent::messages();
+        return [
+            'secao_1.required' => 'O campo da Seção 1 é obrigatório',
+            'secao_1.string' => 'O campo da Seção 1 é inválido',
+            
+            'secao_2.required' => 'O campo da Seção 2 é obrigatório',
+            'secao_2.string' => 'O campo da Seção 2 é inválido',
+            
+            'secao_3.required' => 'O campo da Seção 3 é obrigatório',
+            'secao_3.string' => 'O campo da Seção 3 é inválido',
+            
+            'secao_4.required' => 'O campo da Seção 4 é obrigatório',
+            'secao_4.string' => 'O campo da Seção 4 é inválido',
+            
+            'secao_5.required' => 'O campo da Seção 5 é obrigatório',
+            'secao_5.string' => 'O campo da Seção 5 é inválido',
+            
+            'secao_6.required' => 'O campo da Seção 6 é obrigatório',
+            'secao_6.string' => 'O campo da Seção 6 é inválido',
+            
+            'secao_7.required' => 'O campo da Seção 7 é obrigatório',
+            'secao_7.string' => 'O campo da Seção 7 é inválido',
+            
+            'secao_8.required' => 'O campo da Seção 8 é obrigatório',
+            'secao_8.string' => 'O campo da Seção 8 é inválido',
+            
+            'secao_9.required' => 'O campo da Seção 9 é obrigatório',
+            'secao_9.string' => 'O campo da Seção 9 é inválido',
+            
+            'secao_10.required' => 'O campo da Seção 10 é obrigatório',
+            'secao_10.string' => 'O campo da Seção 10 é inválido',
+            
+
+        ];
     
     }
 }
