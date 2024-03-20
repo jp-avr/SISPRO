6a4688dcca7a531467e6aec6049368fcf72aa056
João Pedro Azevedo Veras dos Reis <joaopedroazevedoreisveras@gmail.com>
2024-03-19 21:33:24 -0300

    criando um dashboard interativo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,141 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <!-- Código de incorporação do Power BI -->
-                    {{-- <iframe width="800" height="600" src="https://app.powerbi.com/view?r=xxxxxxxxxx" frameborder="0" allowFullScreen="true"></iframe> --}}
-
-                    {{ __('You are logged in!') }}
+    <div class="cont">
+        <!-- começo do aside -->
+        <aside>
+            {{-- <div class="topo">
+                <div class="logo">
+                    <h2>C<span class="danger">BABA</span></h2>
                 </div>
+                <div class="fechar">
+                    <span><i data-feather="x"></i></span>
+                </div>
+            </div> --}}
+        </aside>
+        <!-- fim do aside -->
+
+        <!-- seção main -->
+        <main class="dashboard">
+            <h1 class="dashboardh1">Dashboard</h1>
+            <div class="datar">
+                <input type="date">
             </div>
+
+            <div class="insight">
+
+                <!-- COMEÇO DE VENDAS -->
+                <div class="vendas">
+                    <span class="material-symbols-outlined">trending_up</span>
+                    <div class="meio">
+                        <div class="esquerda">
+                            <h3 class="dashboardh1">Vendas Totais</h3>
+                            <h1 class="dashboardh1">$25.0</h1>
+                        </div>
+                        <div class="progresso">
+                            <svg>
+                                <circle r="30" cy="40" cx="40"></circle>
+                            </svg>
+                            <div class="numero">80%</div>
+                        </div>
+                    </div>
+                    <small class="dashboardh1">últimas 24h</small>
+                </div>
+                <!-- FINAL DE VENDAS -->
+
+                <!-- COMEÇO DE CUSTOS -->
+                <div class="custos">
+                    <span class="material-symbols-outlined">shopping_bag</span>
+                    <div class="meio">
+                        <div class="esquerda">
+                            <h3 class="dashboardh1">Despesas</h3>
+                            <h1 class="dashboardh1">$25.0</h1>
+                        </div>
+                        <div class="progresso">
+                            <svg class="teste">
+                                <circle r="30" cy="40" cx="40"></circle>
+                            </svg>
+                            <div class="numero">80%</div>
+                        </div>
+                    </div>
+                    <small class="dashboardh1">últimas 24h</small>
+                </div>
+                <!-- FINAL DE CUSTOS -->
+
+                <!-- COMEÇO DE RECEBIDO -->
+                <div class="recebido">
+                    <span class="material-symbols-outlined">stacked_line_chart</span>
+                    <div class="meio">
+                        <div class="esquerda">
+                            <h3 class="dashboardh1">Recebido</h3>
+                            <h1 class="dashboardh1">$25.0</h1>
+                        </div>
+                        <div class="progresso">
+                            <svg>
+                                <circle r="30" cy="40" cx="40"></circle>
+                            </svg>
+                            <div class="numero">100%</div>
+                        </div>
+                    </div>
+                    <small class="dashboardh1">últimas 24h</small>
+                </div>
+                <!-- FINAL DE RECEBIDO -->
+            </div>
+            <!-- FINAL DE INSIGHTS -->
+            <!-- COMEÇO DE PEDIDOS RECENTES -->
+            <div class="pedidos_recentes">
+                <h1 class="dashboardh1">Processos Recentes</h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nome do Cliente</th>
+                            <th>Número do Processo</th>
+                            <th>Data</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>João</td>
+                            <td>123</td>
+                            <td>Hoje</td>
+                            <td class="warning">Pendente</td>
+                            <td class="primary">Detalhes</td>
+                        </tr>
+                        <tr>
+                            <td>Victor</td>
+                            <td>456</td>
+                            <td>Hoje</td>
+                            <td class="warning">Pendente</td>
+                            <td class="primary">Detalhes</td>
+                        </tr>
+                        <tr>
+                            <td>Sebastião</td>
+                            <td>789</td>
+                            <td>Hoje</td>
+                            <td class="warning">Pendente</td>
+                            <td class="primary">Detalhes</td>
+                        </tr>
+                        <tr>
+                            <td>Ana Julia</td>
+                            <td>1234</td>
+                            <td>Hoje</td>
+                            <td class="warning">Pendente</td>
+                            <td class="primary">Detalhes</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- FINAL DE PEDIDOS RECENTES -->
+
+
+        </main>
+        <!-- fim da seção main -->
+
+        <!-- começo da seção main -->
+        <div class="direito">
+            <h1>Direito</h1>
         </div>
+        <!-- fim da seção direita -->
     </div>
-</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,14 +15,16 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+    
 
     <!-- Styles -->
 
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
     <link href="{{ asset('css/adminkit.css')}}" rel="stylesheet">
 
     
